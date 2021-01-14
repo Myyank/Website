11551b5aee230bd7de761c43679030cb37a13904
Myyank <75198811+Myyank@users.noreply.github.com>
2021-01-14 14:00:43 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -36,3 +36,14 @@
         target: "#sideNav",
     });
 })(jQuery); 
+
+document.querySelector(".toggle").addEventListener("click", function () {
+    this.classList.add("animate");
+  
+    setTimeout(() => {
+      this.classList.toggle("active");
+      document.querySelector(".wave").classList.toggle("active");
+    }, 150);
+  
+    setTimeout(() => this.classList.remove("animate"), 300);
+  });
